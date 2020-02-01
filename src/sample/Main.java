2807dfd85ab5cea8540911ce1859ff75479adc4d
Andrew Whitney <andrewwhitney689@gmail.com>
2020-02-01 11:11:52 -0500

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        AnchorPane root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
+        HBox hotBar = FXMLLoader.load(getClass().getResource("hotBar.fxml"));
+        VBox menu = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        root.getChildren().addAll(menu, hotBar);
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
